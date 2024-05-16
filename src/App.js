f75e41c25cd39dd4ac33c8d7a7fe355f5d4f06c1
Kalipochi <bed-com-01-21@unima.ac.mw>
2024-05-16 23:12:56 +0200

    A new folder is created for easy routing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import React, { useState,useEffect } from "react";
-import { Grid, ThemeProvider } from "@material-ui/core";
- import Theme from "./theme/theme";
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
-import JobCards from "./components/Job/JobCards";
-import NewjobModal from "./components/Job/NewjobModal";
-import jobData from './dummyData';
-//import { AppsOutlined } from "@material-ui/icons";
-//import { firebase} from  "./firebase/Config";
-//import { id } from "date-fns/locale";
+import JobList from './pages/JobList'
+import React from 'react'
 
- 
-
- 
-export default () => {
-  
-  const [newJobModal, setNewJobModal] = useState(false);
- 
-   useEffect(() =>{
-            
- },[]);
-  return ( 
-    
-  <ThemeProvider theme={Theme}>
-    <Header openNewJobModal={() => setNewJobModal(true)} />
-     <NewjobModal closeModal={() => setNewJobModal(false)} newJobModal={newJobModal} />
-    
-    <Grid container justifyContent="center">
-      <Grid item xs={10}>
-
-          <SearchBar />
-
-         {jobData.map((job)=> (
-          <JobCards Key={job.id} {...job} />
-
-          ))} 
-                     
-      </Grid>
-    </Grid>
-  </ThemeProvider>
-  );
-};
+export default function App() {
+  return(
+    <div>
+      <JobList></JobList>
+      
+    </div>
+  )
+}
---
+++ src/pages/JobList.jsx
@@ -0,0 +1,43 @@
+import React, { useState,useEffect } from "react";
+import { Grid, ThemeProvider } from "@material-ui/core";
+ import Theme from "../theme/theme";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import JobCards from "../components/Job/JobCards";
+import NewjobModal from "../components/Job/NewjobModal";
+import jobData from '../dummyData';
+//import { AppsOutlined } from "@material-ui/icons";
+//import { firebase} from  "./firebase/Config";
+//import { id } from "date-fns/locale";
+
+ 
+
+ 
+export default () => {
+  
+  const [newJobModal, setNewJobModal] = useState(false);
+ 
+   useEffect(() =>{
+            
+ },[]);
+  return ( 
+    
+  <ThemeProvider theme={Theme}>
+    <Header openNewJobModal={() => setNewJobModal(true)} />
+     <NewjobModal closeModal={() => setNewJobModal(false)} newJobModal={newJobModal} />
+    
+    <Grid container justifyContent="center">
+      <Grid item xs={10}>
+
+          <SearchBar />
+
+         {jobData.map((job)=> (
+          <JobCards Key={job.id} {...job} />
+
+          ))} 
+                     
+      </Grid>
+    </Grid>
+  </ThemeProvider>
+  );
+};
