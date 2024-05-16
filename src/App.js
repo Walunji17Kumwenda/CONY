a53f53074e74d8b09e1c1bcb05fc25166ee544eb
“Walunji17Kumwenda” <bsc-com-38-22@unima.ac.mw>
2024-05-16 14:25:32 +0200

    react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import JobMatchingAlgorithm from "./Algorithm/JobMatchingAlgorithm";
+
+
+
+function App()
+{
+  return(
+  <div className= "Apply">
+   <JobMatchingAlgorithm userRequirements="experienced in java and python"/>
+   
+       </div>
+
+);
+}
+export default App;
