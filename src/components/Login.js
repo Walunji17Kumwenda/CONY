35f3e4ae63fc74916bf08b784570ddb99d49b872
SYDNEY MTIMA <mtimasydney@gmail.com>
2024-05-24 13:05:30 +0200

    added a login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,63 +1,41 @@
-import {React, useState} from "react";
+import {React} from "react";
+import { useForm } from "react-hook-form";
 
 function Login(){
+    const {register, handleSubmit,formState:{errors}} = useForm();
 
-    const[email,setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    const[errors, setErrors] = useState({});
-
-    const handleEmailChange = (event) =>{
-        setEmail(event.target.value);
-    };
-    const handlePasswordChange = (event) =>{
-        setPassword(event.target.value);
-    };
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-
-        setErrors({});
-
-        const errors ={};
-
-        if(!email){
-            errors.email = 'email is required';
-        }
-         if(!password){
-            errors.password = 'password is required';
-        }else if(password.length < 8){
-            errors.password = 'password must be atleast 8 characters long'
-        }
-
-       /* if(object.keys(errors).length >0){
-            setErrors(errors);
-        }else{
-
-        }*/
-    };
+    const onSubmit = data =>{
+        console.log(data);
+    }; 
     
 
     return(
         <div className="flex h-screen flex-flow items-center justify-center">
         <div className="flex flex-flow items-center justify-center  rounded-xl border-3 bg-gray-200 h-[500px] w-[420px]">
-        <form onSubmit={handleSubmit}>
-            <h1 className="text-[36px] text-center">Login</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-[36px] mb-5 text-center">Login</h1>
             <div className=" mt-4 relative m-[30px 0]">      
-             <input type="text" placeholder="Email" value={email} onChange={handleEmailChange} className="p-2 w-full border-none outline-none rounded-xl "/>
-             {errors.email && <p>{errors.email}</p>}
+             <input type="email" placeholder="Email" name="email" {...register("email",{required: true, pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i})} className="p-2 w-full border-none outline-none rounded-xl "/>
+             
+             {errors.email?.type==="required" && <p className="text-red-500">email is required</p>}
+             {errors.email?.type==="pattern" && <p className="text-red-500">email pattern is wrong</p>}
+             
             </div>
             <div className="mt-4 relative  m-[30px 0]">
-                <input type="password" placeholder="password" value={password} onChange={handlePasswordChange} className="p-2 w-full rounded-xl outline-none border-none"/>
-                {errors.password && <p>{errors.password}</p>}
+                <input type="password" placeholder="password" name="password" {...register("password",{required:true, minLength: 8, maxLength:20,})} className="p-2 w-full rounded-xl outline-none border-none"/>
+                {errors.password?.type === "minLength" && <p className="text-red-500">password must be atleast 8 characters </p>}
+                {errors.password?.type === "maxLength" && <p className="text-red-500">password must be less than 20 characters </p>}
             </div>
+                
             <div className="flex items-center justify-between gap-4 mt-4 ">
                 <label><input type="checkbox"/> Remember me</label>
                 <a href="#">forgot password?</a>
             </div>
             <div className="flex items-center justify-center mt-4">
-                <button type="submit"  className="bg-blue-950 text-orange-500 w-full p-2 rounded-xl font-bold">Sign in</button>
+              <button type="submit"  className="bg-blue-950 hover:bg-blue-900 transtion-all duration-500 text-orange-500 py-2 px-10 w-full  rounded-xl font-bold">Sign in</button>
             </div>
             <div className="flex items-center justify-center mt-4 font-semibold">
-                <p>Dont have an account? <a href="#">Sign up</a></p>
+                <p>Dont have an account? <a href="#">Sign up</a> </p>
             </div>
 
         </form>
